1ed5200e623ce441e55fff622e813e1734bd55b3
ashkan <ashkan.ram70@gmail.com>
2019-11-04 21:21:55 -0800

    fixing heroku bug

--- server.js
+++ server.js
@@ -8,10 +8,21 @@ var db = require("./models");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
+
+
+// Serve up static assets (usually on heroku)
+/* if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+} */
 // Add routes, both API and view
 app.use(routes);
 
