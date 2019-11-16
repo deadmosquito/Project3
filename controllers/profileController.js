f8928b7a3a00aa19838dd2a3935cd3034436231f
Alex Shoemaker <iamalexshoemaker@gmail.com>
2019-11-16 12:46:10 -0800

    added profile page and update profile functionality

---
+++ controllers/profileController.js
@@ -0,0 +1,38 @@
+const db = require("../models");
+
+
+module.exports = {
+    findOne: function(req, res) {
+      console.log(req.session);
+      if(req.session.isAuthorLoggin ===false){
+        let data = {
+          isSuccess: "No"
+        }
+        return res.json(data)
+      }
+      db.Author
+        .findOne({where: {id: req.session.authorId}})
+        .then((dbModel) =>{
+          res.json(dbModel)} )
+        .catch(err => res.status(422).json(err));
+    },
+    update: function(req, res) { 
+      console.log(req.body)
+      db.Author
+      .update(
+        {
+          fname: req.body.fname,
+          lname: req.body.lname,
+          email: req.body.email,
+          githubURL: req.body.githubURL,
+          profileURL: req.body.profileURL
+        },
+        {returning: true, where: {id: req.body.id} }
+      )
+        .then((dbModel) => {  
+          res.json(dbModel) 
+        })
+        .catch(err => res.status(422).json(err));
+
+    }
+}
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
-const newPost = require("./new-post")
-const registration = require("./registration")
-const login = require("./login")
-const home = require("./home")
-const news = require("./news")
-const logout = require("./logout")
+const newPost = require("./new-post");
+const registration = require("./registration");
+const login = require("./login");
+const home = require("./home");
+const news = require("./news");
+const logout = require("./logout");
+const profile = require("./profile");
+
 
 router.use("/new-post", newPost)
 router.use("/registration", registration)
@@ -12,5 +14,6 @@ router.use("/login", login)
 router.use('/home',home)
 router.use("/news", news)
 router.use("/logout", logout)
+router.use("/profile", profile)
 
 module.exports = router;
